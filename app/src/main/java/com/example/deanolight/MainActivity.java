b25ba677932fbbb84c03ef50f9a57ecfaedd7f1a
DeanoNoire <rykala.dan@gmail.com>
2020-09-25 22:12:07 +0200

    working core of app

--- app/src/main/java/com/example/deanolight/MainActivity.java
+++ app/src/main/java/com/example/deanolight/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,34 +24,37 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView mainLightImg;
     ImageView secondaryLightImage;
-    public static String wifiModuleIp = "";
-    public static int wifiModulePort = 0;
-    public static String CMD = "0";
+    String mainLightStatus;
+    String secondaryLightStatus;
+    public static String wifiModuleIp = "192.168.1.112";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sendGetRequest();
-
+        zjistiStatus();
         mainLightImg = (ImageView) findViewById(R.id.mainLightImage);
         secondaryLightImage = (ImageView) findViewById(R.id.secondaryLightImage);
 
         mainLightImg.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //sendGetRequest
+                zmenStav("mainLight");
             }
         });
 
-
-
+        secondaryLightImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                zmenStav("secondaryLight");
+            }
+        });
     }
 
-    private void sendGetRequest(){
+    private void zjistiStatus(){
         RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
-        String url="http://www.gist.cz/zakaznici/DR/stav.json";
+        String url= "http://"+wifiModuleIp+"/rest_status";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -75,23 +79,58 @@ public class MainActivity extends AppCompatActivity {
 
     private void stavSvetel(String response) throws JSONException {
 
-
             JSONObject jObject = new JSONObject(response);
-            String mainValue = jObject.getString("main");
+            String mainValue = jObject.getString("mainLight");
             Log.i("Main: ",mainValue);
-            String secondaryValue = jObject.getString("secondary");
+            String secondaryValue = jObject.getString("secondaryLight");
             Log.i("Secondary: ",secondaryValue);
 
-            if(mainValue.equals("1")){
+            if(mainValue.equals("0")){
                 mainLightImg.setImageResource(R.drawable.on);
+                mainLightStatus = "off";
             }
-            else {mainLightImg.setImageResource(R.drawable.off);}
+            else {mainLightImg.setImageResource(R.drawable.off);
+                mainLightStatus = "on";
+            }
 
-        if(secondaryValue.equals("1")){
+        if(secondaryValue.equals("0")){
             secondaryLightImage.setImageResource(R.drawable.on);
+            secondaryLightStatus = "off";
         }
-        else {secondaryLightImage.setImageResource(R.drawable.off);}
+        else {secondaryLightImage.setImageResource(R.drawable.off);
+              secondaryLightStatus = "on";
+        }
+     }
 
+     private void zmenStav(String svetlo){
+         RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+         String status = zjistiStatusRychly(svetlo);
+         String url= "http://"+wifiModuleIp+"/"+svetlo+"/"+status;
+         Log.i("URL: ",url);
+         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+         @Override
+         public void onResponse(String response) {
+             zjistiStatus();
+             }
+     }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("error",error.toString());
+        }
+    });
+      queue.add(stringRequest);
+     }
+
+     private String zjistiStatusRychly(String svetlo){
+        if(svetlo.equals("mainLight")){
+            return mainLightStatus ;
+        }
+        else if(svetlo.equals("secondaryLight")){
+            return secondaryLightStatus;
+        }
+        else {
+            return "error";
+        }
         }
 
 
